bd05cf14199cddd08e1573aff2d9fc231b38d543
nastia-vovk <1103192@gmail.com>
2017-11-22 20:54:10 +0200

    made Actions and Menus

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -1,8 +1,8 @@
-coding: utf-8
+-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
 
-  class Course(models.Model):
+class Course(models.Model):
     _name = 'openacademy.course'
 
     name = fields.Char(string="Title", required=True)
