dff6e930220202293158fc0b94f4d9fb3402d2d3
nastia-vovk <1103192@gmail.com>
2017-11-22 22:02:54 +0200

    make Relations between models

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -11,6 +11,9 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
+    responsible_id = fields.Many2one('res.users',
+        ondelete='set null', string="Responsible", index=True)
+
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -19,3 +22,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    course_id = fields.Many2one('openacademy.course',
+                                ondelete='cascade', string="Course", required=True)
